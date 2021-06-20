3842a4da566bd915eb69c16b3ad64412a53f4af0
effe-m <francesco.megna97@gmail.com>
2021-06-20 15:44:59 +0200

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  purge: ["'./src/**/*.{js,ts,jsx,tsx}'"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-};
+	purge: [
+			'./pages/**/*.{js,ts,jsx,tsx}',
+			'./components/**/*.{js,ts,jsx,tsx}',
+			'./layout/**/*.{js,ts,jsx,tsx}',
+			'./helpers/**/*.{js,ts,jsx,tsx}',
+	],
+	darkMode: 'class',
+	theme: {
+			extend: {},
+	},
+	variants: {
+			extend: {},
+	},
+	plugins: [],
+}
